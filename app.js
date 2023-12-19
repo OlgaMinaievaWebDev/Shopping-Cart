732092114c3a0163ee4f0a993cd0bcef1303fd12
Olga <minaeva9@gmail.com>
2023-12-19 15:15:34 -0500

    function add item to list

--- app.js
+++ app.js
@@ -15,11 +15,22 @@ const shoppingListDB = ref(database, "shoppingList");
 
 const inputEl = document.getElementById("input");
 const addBtn = document.getElementById("add");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addBtn.addEventListener("click", function () {
   let inputValue = inputEl.value;
 
   push(shoppingListDB, inputValue);
 
-  console.log(inputValue);
+  clearInputEl();
+
+  addItem(inputValue);
 });
+
+function clearInputEl() {
+  inputEl.value = "";
+}
+
+function addItem(item) {
+  shoppingListEl.innerHTML += `<li>${item}</li>`;
+}
