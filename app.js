18c95911ca84d0ef48109a9774e6a9616d5e0b06
Olga <minaeva9@gmail.com>
2023-12-19 17:38:51 -0500

    function clear input, clearList added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import {
   getDatabase,
   ref,
   push,
+  onValue,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
@@ -17,20 +18,39 @@ const inputEl = document.getElementById("input");
 const addBtn = document.getElementById("add");
 const shoppingListEl = document.getElementById("shopping-list");
 
+//button fire
 addBtn.addEventListener("click", function () {
   let inputValue = inputEl.value;
 
   push(shoppingListDB, inputValue);
 
   clearInputEl();
-
-  addItem(inputValue);
 });
 
+//fetching data from DB
+onValue(shoppingListDB, function (snapshot) {
+  let itemsArr = Object.values(snapshot.val());
+  clearShoppingList();
+  for (let i = 0; i < itemsArr.length; i++) {
+    addItem(itemsArr[i]);
+  }
+});
+
+
+
+//clear shopping list
+function clearShoppingList() {
+  shoppingListEl.innerHTML = "";
+}
+
+
+
+//clear input field
 function clearInputEl() {
   inputEl.value = "";
 }
 
+//add items to list
 function addItem(item) {
   shoppingListEl.innerHTML += `<li>${item}</li>`;
 }
