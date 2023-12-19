4fa78934c9efa653661f3f3f4e798c217ce67890
Olga <minaeva9@gmail.com>
2023-12-19 14:58:01 -0500

    export DB

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
 const appSettings = {
   databaseURL: "https://test-531ea-default-rtdb.firebaseio.com/",
 };
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListDB = ref(database, "shoppingList");
 
 const inputEl = document.getElementById("input");
 const addBtn = document.getElementById("add");
 
 addBtn.addEventListener("click", function () {
   let inputValue = inputEl.value;
+
+  push(shoppingListDB, inputValue);
+
   console.log(inputValue);
 });
